4651ad61b11b890703106d49809aaf58a057e8f2
SaturnoXVI <130761712+SaturnoXVI@users.noreply.github.com>
2023-11-17 09:27:04 -0500

    a

---
+++ db.sql
@@ -0,0 +1,593 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Tiempo de generación: 17-11-2023 a las 14:23:18
+-- Versión del servidor: 8.0.30
+-- Versión de PHP: 7.3.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `redsocial1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `actividad_usuario`
+--
+
+CREATE TABLE `actividad_usuario` (
+  `id` int NOT NULL,
+  `usuario_id` int DEFAULT NULL,
+  `tipo_actividad` varchar(50) DEFAULT NULL,
+  `fecha_actividad` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `amistades`
+--
+
+CREATE TABLE `amistades` (
+  `id` int NOT NULL,
+  `usuario_id_1` int DEFAULT NULL,
+  `usuario_id_2` int DEFAULT NULL,
+  `estado` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asistentes_evento`
+--
+
+CREATE TABLE `asistentes_evento` (
+  `id` int NOT NULL,
+  `usuario_id` int DEFAULT NULL,
+  `evento_id` int DEFAULT NULL,
+  `fecha_de_asistencia` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `bloqueos`
+--
+
+CREATE TABLE `bloqueos` (
+  `id` int NOT NULL,
+  `usuario_id` int DEFAULT NULL,
+  `usuario_bloqueado_id` int DEFAULT NULL,
+  `fecha_bloqueo` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `busquedas`
+--
+
+CREATE TABLE `busquedas` (
+  `id` int NOT NULL,
+  `usuario_id` int DEFAULT NULL,
+  `consulta` text,
+  `fecha_busqueda` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `id` int NOT NULL,
+  `usuario_id` int DEFAULT NULL,
+  `publicacion_id` int DEFAULT NULL,
+  `contenido` text,
+  `fecha_de_comentario` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `eventos`
+--
+
+CREATE TABLE `eventos` (
+  `id` int NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `descripcion` text,
+  `fecha` date DEFAULT NULL,
+  `hora` time DEFAULT NULL,
+  `ubicacion` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `grupos`
+--
+
+CREATE TABLE `grupos` (
+  `id` int NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `descripcion` text,
+  `fecha_de_creacion` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `foto_grupo` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mensajes_privados`
+--
+
+CREATE TABLE `mensajes_privados` (
+  `id` int NOT NULL,
+  `de_usuario_id` int DEFAULT NULL,
+  `para_usuario_id` int DEFAULT NULL,
+  `contenido` text,
+  `fecha_de_envio` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `me_gusta`
+--
+
+CREATE TABLE `me_gusta` (
+  `id` int NOT NULL,
+  `usuario_id` int DEFAULT NULL,
+  `publicacion_id` int DEFAULT NULL,
+  `tipo_reaccion` varchar(50) DEFAULT NULL,
+  `fecha_reaccion` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `miembros_grupo`
+--
+
+CREATE TABLE `miembros_grupo` (
+  `id` int NOT NULL,
+  `usuario_id` int DEFAULT NULL,
+  `grupo_id` int DEFAULT NULL,
+  `fecha_de_unirse` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `notificaciones`
+--
+
+CREATE TABLE `notificaciones` (
+  `id` int NOT NULL,
+  `usuario_id` int DEFAULT NULL,
+  `tipo_notificacion` varchar(50) DEFAULT NULL,
+  `contenido` text,
+  `fecha_creacion` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `publicaciones`
+--
+
+CREATE TABLE `publicaciones` (
+  `id` int NOT NULL,
+  `usuario_id` int DEFAULT NULL,
+  `contenido` text,
+  `fecha_de_publicacion` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `publicaciones_compartidas`
+--
+
+CREATE TABLE `publicaciones_compartidas` (
+  `id` int NOT NULL,
+  `usuario_id` int DEFAULT NULL,
+  `publicacion_original_id` int DEFAULT NULL,
+  `comentario` text,
+  `fecha_de_compartir` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int NOT NULL,
+  `nombre` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id`, `nombre`) VALUES
+(1, 'Administrador'),
+(2, 'Moderador'),
+(3, 'Usuario');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL,
+  `nombre` varchar(50) DEFAULT NULL,
+  `apellido` varchar(50) DEFAULT NULL,
+  `correo_electronico` varchar(100) DEFAULT NULL,
+  `contrasena` varchar(255) DEFAULT NULL,
+  `fecha_de_registro` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `foto_perfil` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios_roles`
+--
+
+CREATE TABLE `usuarios_roles` (
+  `id` int NOT NULL,
+  `usuario_id` int DEFAULT NULL,
+  `rol_id` int DEFAULT NULL,
+  `fecha_asignacion` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `actividad_usuario`
+--
+ALTER TABLE `actividad_usuario`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indices de la tabla `amistades`
+--
+ALTER TABLE `amistades`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id_1` (`usuario_id_1`),
+  ADD KEY `usuario_id_2` (`usuario_id_2`);
+
+--
+-- Indices de la tabla `asistentes_evento`
+--
+ALTER TABLE `asistentes_evento`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `evento_id` (`evento_id`);
+
+--
+-- Indices de la tabla `bloqueos`
+--
+ALTER TABLE `bloqueos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `usuario_bloqueado_id` (`usuario_bloqueado_id`);
+
+--
+-- Indices de la tabla `busquedas`
+--
+ALTER TABLE `busquedas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `publicacion_id` (`publicacion_id`);
+
+--
+-- Indices de la tabla `eventos`
+--
+ALTER TABLE `eventos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `grupos`
+--
+ALTER TABLE `grupos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `mensajes_privados`
+--
+ALTER TABLE `mensajes_privados`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `de_usuario_id` (`de_usuario_id`),
+  ADD KEY `para_usuario_id` (`para_usuario_id`);
+
+--
+-- Indices de la tabla `me_gusta`
+--
+ALTER TABLE `me_gusta`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `publicacion_id` (`publicacion_id`);
+
+--
+-- Indices de la tabla `miembros_grupo`
+--
+ALTER TABLE `miembros_grupo`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `grupo_id` (`grupo_id`);
+
+--
+-- Indices de la tabla `notificaciones`
+--
+ALTER TABLE `notificaciones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indices de la tabla `publicaciones`
+--
+ALTER TABLE `publicaciones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indices de la tabla `publicaciones_compartidas`
+--
+ALTER TABLE `publicaciones_compartidas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `publicacion_original_id` (`publicacion_original_id`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `correo_electronico` (`correo_electronico`);
+
+--
+-- Indices de la tabla `usuarios_roles`
+--
+ALTER TABLE `usuarios_roles`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `rol_id` (`rol_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `actividad_usuario`
+--
+ALTER TABLE `actividad_usuario`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `amistades`
+--
+ALTER TABLE `amistades`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `asistentes_evento`
+--
+ALTER TABLE `asistentes_evento`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `bloqueos`
+--
+ALTER TABLE `bloqueos`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `busquedas`
+--
+ALTER TABLE `busquedas`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `eventos`
+--
+ALTER TABLE `eventos`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `grupos`
+--
+ALTER TABLE `grupos`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `mensajes_privados`
+--
+ALTER TABLE `mensajes_privados`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `me_gusta`
+--
+ALTER TABLE `me_gusta`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `miembros_grupo`
+--
+ALTER TABLE `miembros_grupo`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `notificaciones`
+--
+ALTER TABLE `notificaciones`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `publicaciones`
+--
+ALTER TABLE `publicaciones`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `publicaciones_compartidas`
+--
+ALTER TABLE `publicaciones_compartidas`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios_roles`
+--
+ALTER TABLE `usuarios_roles`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `actividad_usuario`
+--
+ALTER TABLE `actividad_usuario`
+  ADD CONSTRAINT `actividad_usuario_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `amistades`
+--
+ALTER TABLE `amistades`
+  ADD CONSTRAINT `amistades_ibfk_1` FOREIGN KEY (`usuario_id_1`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `amistades_ibfk_2` FOREIGN KEY (`usuario_id_2`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `asistentes_evento`
+--
+ALTER TABLE `asistentes_evento`
+  ADD CONSTRAINT `asistentes_evento_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `asistentes_evento_ibfk_2` FOREIGN KEY (`evento_id`) REFERENCES `eventos` (`id`);
+
+--
+-- Filtros para la tabla `bloqueos`
+--
+ALTER TABLE `bloqueos`
+  ADD CONSTRAINT `bloqueos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `bloqueos_ibfk_2` FOREIGN KEY (`usuario_bloqueado_id`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `busquedas`
+--
+ALTER TABLE `busquedas`
+  ADD CONSTRAINT `busquedas_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `comentarios_ibfk_2` FOREIGN KEY (`publicacion_id`) REFERENCES `publicaciones` (`id`);
+
+--
+-- Filtros para la tabla `mensajes_privados`
+--
+ALTER TABLE `mensajes_privados`
+  ADD CONSTRAINT `mensajes_privados_ibfk_1` FOREIGN KEY (`de_usuario_id`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `mensajes_privados_ibfk_2` FOREIGN KEY (`para_usuario_id`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `me_gusta`
+--
+ALTER TABLE `me_gusta`
+  ADD CONSTRAINT `me_gusta_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `me_gusta_ibfk_2` FOREIGN KEY (`publicacion_id`) REFERENCES `publicaciones` (`id`);
+
+--
+-- Filtros para la tabla `miembros_grupo`
+--
+ALTER TABLE `miembros_grupo`
+  ADD CONSTRAINT `miembros_grupo_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `miembros_grupo_ibfk_2` FOREIGN KEY (`grupo_id`) REFERENCES `grupos` (`id`);
+
+--
+-- Filtros para la tabla `notificaciones`
+--
+ALTER TABLE `notificaciones`
+  ADD CONSTRAINT `notificaciones_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `publicaciones`
+--
+ALTER TABLE `publicaciones`
+  ADD CONSTRAINT `publicaciones_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `publicaciones_compartidas`
+--
+ALTER TABLE `publicaciones_compartidas`
+  ADD CONSTRAINT `publicaciones_compartidas_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `publicaciones_compartidas_ibfk_2` FOREIGN KEY (`publicacion_original_id`) REFERENCES `publicaciones` (`id`);
+
+--
+-- Filtros para la tabla `usuarios_roles`
+--
+ALTER TABLE `usuarios_roles`
+  ADD CONSTRAINT `usuarios_roles_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `usuarios_roles_ibfk_2` FOREIGN KEY (`rol_id`) REFERENCES `roles` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
